360a392d74bf604085ff787f8f9cbb1f2f4aa72f
Michalis Anastasiou <mc.anastasiou@gmail.com>
2018-12-15 22:14:22 +0200

    Edited header

--- rick-and-morty/src/app/app.module.ts
+++ rick-and-morty/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { CharactersModule } from './modules/characters/characters.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    CharactersModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- rick-and-morty/src/app/core/core.module.ts
+++ rick-and-morty/src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent]
